92e1a67688228c40f1da38d8c5691e8c90c0b126
Kitamura Satoshi <with.no.parachute@gmail.com>
2013-01-21 11:09:52 +0900

    add experimental Form

---
+++ Form/Model/Paginator.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Contrib\PaginatorBundle\Form\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Paginator
+{
+    /**
+     * @var int
+     *
+     * @Assert\Type(type="integer")
+     * @Assert\Range(min=1)
+     */
+    private $page;
+
+    /**
+     * @var int
+     *
+     * @Assert\Type(type="integer")
+     * @Assert\Range(min=1)
+     */
+    private $size;
+
+    public function setPage($page)
+    {
+        $this->page = $page;
+
+        return $this;
+    }
+
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    public function setSize($size)
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    public function getSize()
+    {
+        return $this->size;
+    }
+}
---
+++ Form/PaginatorType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Contrib\PaginatorBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class PaginatorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('page', null)
+        ->add('size', null)
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $defaultValues = array(
+            'data_class' => 'Contrib\PaginatorBundle\Form\Model\Paginator',
+        );
+
+        $resolver->setDefaults($defaultValues);
+    }
+
+    public function getName()
+    {
+        return '';
+    }
+
+    public static function filterRequest(Request $request, array $default = array())
+    {
+        $default += array(
+            'page' => 1,
+            'size' => 20,
+        );
+
+        return array(
+            'page' => $request->get('page', $default['page']),
+            'size' => $request->get('size', $default['size']),
+        );
+    }
+}
